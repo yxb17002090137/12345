d511eba3f3357f8a9a5deccebecd048ecb01a0dd
Administrator <444@qq.com>
2018-09-27 09:01:33 +0800

    1

---
+++ Elephant.java
@@ -0,0 +1,15 @@
+public class Elephant extends Animal{
+
+public Elephant() {}
+
+public Elephant(String weight) {
+	this.weight=weight;
+}
+
+public String getweight() {
+	this.weight=weight; ;
+}
+
+public String setweight() {
+	return height;
+}
