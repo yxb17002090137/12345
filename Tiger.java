d632238821c37a3ffe5f1c7fce6af22bab745b54
Xgh17002090135 <dsaihda@qq.com>
2018-09-27 08:45:47 +0800

    yxb

---
+++ Tiger.java
@@ -0,0 +1,14 @@
+public class Tiger extends Animal{
+    private String food;
+
+    public Tiger() {
+    }
+    public Tiger(String food) {
+        this.food=food;
+    }
+    public String getfood() {
+        this.food=food; ;
+    }
+    public String setfood() {
+        return food;
+    }
