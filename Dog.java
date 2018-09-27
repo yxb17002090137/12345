4142c88d9f6c30cf9f5f21e6823512d3a0abe3ea
Administrator <132@qq.com>
2018-09-27 08:55:10 +0800

    补充

---
+++ Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Animal{
+    private String height;
+
+    public Dog() {
+    }
+    public Dog(String height) {
+        this.height=height;
+    }
+    public String getheight() {
+        this.height=height; ;
+    }
+    public String setheight() {
+        return height;
+    }
